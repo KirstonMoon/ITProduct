31bbe094135cfb15cb8816e61a1c934af321fcf1
Kirill Magerya <fresno.super@gmail.com>
2021-02-23 00:50:00 -0800

    добавил читаемости коду

--- ITProduct/MainModule/MainViewController.swift
+++ ITProduct/MainModule/MainViewController.swift
@@ -76,15 +76,19 @@ class MainViewController: UIViewController {
 extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
         if mainView.simpleNumbersButton.backgroundColor == #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1) {
             return simpleNumbers.count
+            
         } else if mainView.fibonacciNumbersButton.backgroundColor == #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1) {
             return fibsNumbers.count
         }
+        
         fatalError("Сбой при высчитывании количества элементов в массиве чисел")
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         guard let cell = mainView.collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.cellId, for: indexPath) as? CustomCollectionViewCell else { fatalError("Не удалось создать ячейку") }
         
         if mainView.simpleNumbersButton.backgroundColor == #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1) {
@@ -99,6 +103,7 @@ extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSour
         } else {
             cell.backgroundColor = .systemGray
         }
+        
         return cell
     }
     
